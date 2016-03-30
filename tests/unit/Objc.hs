mainland/language-c-quote

commit a35e6212742eec62210fd5011664e4c0ee9edc41

Flesh out support for Objective C antiquoters.
This work is based on a patch from @mageshb.

Language/C/Quote/Base.hs
@@ -205,12 +205,16 @@ qqDefinitionE (C.AntiEsc v loc) =
     Just [|C.EscDef $(antiVarE v) $(qqLocE loc)|]
 qqDefinitionE (C.AntiEdecl v _) =
     Just $ antiVarE v
+qqDefinitionE (C.AntiObjCMeth m _) =
+    Just $ antiVarE m
 qqDefinitionE _ = Nothing
 
 qqDefinitionListE :: [C.Definition] -> Maybe (Q Exp)
 qqDefinitionListE [] = Just [|[]|]
 qqDefinitionListE (C.AntiEdecls v _ : defs) =
     Just [|$(antiVarE v) ++ $(dataToExpQ qqExp defs)|]
+qqDefinitionListE (C.AntiObjCMeths m _ : meths) =
+    Just [|$(antiVarE m) ++ $(dataToExpQ qqExp meths)|]
 qqDefinitionListE (def : defs) =
     Just [|$(dataToExpQ qqExp def) : $(dataToExpQ qqExp defs)|]
 
@@ -330,6 +334,63 @@ qqObjcIfaceDeclE (C.AntiObjCIfaceDecl v _  : decls) =
 qqObjcIfaceDeclE (decl : decls) =
   Just [|$(dataToExpQ qqExp decl) : $(dataToExpQ qqExp decls)|]
 
+qqObjCPropE :: C.ObjCIfaceDecl -> Maybe (Q Exp)
+qqObjCPropE (C.AntiObjCProp p _) = Just $ antiVarE p
+qqObjCPropE _                    = Nothing
+
+qqObjCPropListE :: [C.ObjCIfaceDecl] -> Maybe (Q Exp)
+qqObjCPropListE [] = Just [|[]|]
+qqObjCPropListE (C.AntiObjCProps p _: props) = Just $ [|$(antiVarE p) ++ $(dataToExpQ qqExp props)|]
+qqObjCPropListE _                            = Nothing
+
+qqObjCPropAttrE :: C.ObjCPropAttr -> Maybe (Q Exp)
+qqObjCPropAttrE (C.AntiObjCAttr pa _) = Just $ antiVarE pa
+qqObjCPropAttrE _                     = Nothing
+
+qqObjCPropAttrListE :: [C.ObjCPropAttr] -> Maybe (Q Exp)
+qqObjCPropAttrListE [] = Just [|[]|]
+qqObjCPropAttrListE (C.AntiObjCAttrs pa _:attrelems) =
+    Just $ [|$(antiVarE pa) ++ $(dataToExpQ qqExp attrelems)|]
+qqObjCPropAttrListE (pattr : pattrs) =
+    Just [|$(dataToExpQ qqExp pattr) : $(dataToExpQ qqExp pattrs)|]
+
+qqObjCDictsE :: [C.ObjCDictElem] -> Maybe (Q Exp)
+qqObjCDictsE [] = Just [|[]|]
+qqObjCDictsE (C.AntiObjCDictElems e _:elems) =
+    Just $ [|$(antiVarE e) ++ $(dataToExpQ qqExp elems)|]
+qqObjCDictsE (elem : elems) =
+    Just [|$(dataToExpQ qqExp elem) : $(dataToExpQ qqExp elems)|]
+
+qqObjCParamE :: C.ObjCParam -> Maybe (Q Exp)
+qqObjCParamE (C.AntiObjCParam p _) = Just $ antiVarE p
+qqObjCParamE _                     = Nothing
+
+qqObjCParamsE :: [C.ObjCParam] -> Maybe (Q Exp)
+qqObjCParamsE [] = Just [|[]|]
+qqObjCParamsE (C.AntiObjCParams p _: props) =
+    Just $ [|$(antiVarE p) ++ $(dataToExpQ qqExp props)|]
+qqObjCParamsE (param : params) =
+    Just [|$(dataToExpQ qqExp param) : $(dataToExpQ qqExp params)|]
+
+qqObjCMethodProtoE :: C.ObjCMethodProto -> Maybe (Q Exp)
+qqObjCMethodProtoE (C.AntiObjCMethodProto p _) = Just $ antiVarE p
+qqObjCMethodProtoE _                           = Nothing
+
+qqObjCRecvE :: C.ObjCRecv -> Maybe (Q Exp)
+qqObjCRecvE (C.AntiObjCRecv p _) = Just $ antiVarE p
+qqObjCRecvE _                  = Nothing
+
+qqObjCArgE :: C.ObjCArg -> Maybe (Q Exp)
+qqObjCArgE (C.AntiObjCArg p _) = Just $ antiVarE p
+qqObjCArgE _                  = Nothing
+
+qqObjCArgsE :: [C.ObjCArg] -> Maybe (Q Exp)
+qqObjCArgsE [] = Just [|[]|]
+qqObjCArgsE (C.AntiObjCArgs a _: args) =
+    Just $ [|$(antiVarE a) ++ $(dataToExpQ qqExp args)|]
+qqObjCArgsE (arg : args) =
+    Just [|$(dataToExpQ qqExp arg) : $(dataToExpQ qqExp args)|]
+
 qqExp :: Typeable a => a -> Maybe (Q Exp)
 qqExp = const Nothing  `extQ` qqStringE
                        `extQ` qqIdE
@@ -356,6 +417,17 @@ qqExp = const Nothing  `extQ` qqStringE
                        `extQ` qqBlockItemE
                        `extQ` qqBlockItemListE
                        `extQ` qqObjcIfaceDeclE
+                       `extQ` qqObjCPropE
+                       `extQ` qqObjCPropListE
+                       `extQ` qqObjCPropAttrE
+                       `extQ` qqObjCPropAttrListE
+                       `extQ` qqObjCDictsE
+                       `extQ` qqObjCParamE
+                       `extQ` qqObjCParamsE
+                       `extQ` qqObjCMethodProtoE
+                       `extQ` qqObjCRecvE
+                       `extQ` qqObjCArgE
+                       `extQ` qqObjCArgsE
 
 antiVarP :: String -> PatQ
 antiVarP = either fail return . parsePat

tests/unit/Main.hs
@@ -10,6 +10,7 @@ import Data.Loc (SrcLoc, noLoc)
 import Language.C.Quote.C
 import qualified Language.C.Syntax as C
 import Numeric (showHex)
+import Objc (objcTests)
 import System.Exit (exitFailure, exitSuccess)
 
 main :: IO ()
@@ -21,6 +22,7 @@ tests = [ constantTests
         , cQuotationTests
         , cPatternAntiquotationTests
         , statementCommentTests
+        , objcTests
         ]
 
 constantTests :: Test

tests/unit/Objc.hs
@@ -1,0 +1,150 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Objc (objcTests) where
+
+import Test.Framework
+import Test.Framework.Providers.HUnit
+import Test.HUnit (Assertion, (@?=))
+
+import Language.C.Quote.ObjC
+
+objcTests :: Test
+objcTests = testGroup "Objective-C"
+    [ testCase "Objective-C params" objcProp
+    , testCase "Objective-C property" objcDict
+    , testCase "Objective-C method parameters" objcParam
+    , testCase "Objective-C method definition" objcMethodDefinition
+    , testCase "Objective-C classmethod" objcArgumentCls
+    , testCase "Objective-C argument" objcArgument
+    , testCase "Objective-C arguments" objcArguments
+    , testCase "Objective-C varargument" objcVarArgument
+    , testCase "Objective-C literals" objcLits
+    ]
+  where
+    objcDict :: Assertion
+    objcDict =
+        [cexp| @{$dictelems:(elems [("a","b"),("c", "d")])} |] @?= [cexp| @{@"a" : @"b",@"c": @"d"}|]
+      where
+        elems = map (\(k,v) -> [objcdictelem|$exp:(objcLit k) : $exp:(objcLit v)|] )
+
+    objcProp :: Assertion
+    objcProp =
+        [cedecl|
+         @interface Foo
+         - (void) foo;
+         $prop:propdec1
+         $props:propdec2
+         $prop:propdec3
+         @end
+         |]
+
+        @?=
+
+        [cedecl|
+        @interface Foo
+        - (void) foo;
+        @property (nonatomic, retain) int i;
+        @property (nonatomic, retain) float j;
+        @property (nonatomic, retain) char k;
+        @property (nonatomic) double l;
+        @end
+        |]
+      where
+        propdec n typ = [objcprop|@property ($propattrs:r) $ty:typ $id:n;|]
+        propdec' n typ = [objcprop|@property ($propattr:p) $ty:typ $id:n;|]
+        p = [objcpropattr|nonatomic|]
+        q = [objcpropattr|retain|]
+        r = [p,q]
+        propdec1 = propdec "i" [cty|int|]
+        propdec2 = map (\(n,t) -> propdec n t) [("j", [cty|float|]), ("k", [cty|char|])]
+        propdec3 = propdec' "l" [cty|double|]
+
+    objcParam :: Assertion
+    objcParam =
+        [cedecl|
+        @interface Foo
+        - (void) $methparams:paramNew ;
+        $methproto:val ;
+        @end
+        |]
+
+        @?=
+
+        [cedecl|
+        @interface Foo
+        - (void) foo:(int)str fo:(int)str1;
+        + (int) test1:(int)str2;
+        @end
+        |]
+      where
+        paramNew1 = [objcmethparam|$id:("foo"):(int)str |]
+        paramNew2 = [objcmethparam|fo:(int)str1 |]
+        paramNew3 = [objcmethparam|test1:(int)str2 |]
+        paramNew = [paramNew1, paramNew2]
+        val = [objcmethproto|+ (int) $methparam:paramNew3|]
+
+    objcMethodDefinition :: Assertion
+    objcMethodDefinition =
+        [cedecl|
+        @implementation fooclass
+        $methdefs:(val)
+        $methdef:(val3)
+        @end
+        |]
+
+        @?=
+
+        [cedecl|
+        @implementation fooclass
+        + (int) test1:(int)foo { }
+        - (char) test2:(char)bar { }
+        + (float) test3:(double)baz { }
+        @end
+        |]
+      where
+        val3 = [objcmethdef|+ (float) $methparam:paramNew5 {} |]
+        paramNew5 = [objcmethparam|test3:(double)baz |]
+        val2 = [objcmethdef|+ (int) $methparam:paramNew3 {} |]
+        paramNew3 = [objcmethparam|test1:(int)foo |]
+        val1 = [objcmethdef|- (char) $methparam:paramNew4 {} |]
+        paramNew4 = [objcmethparam|test2:(char)bar |]
+        val = [val2, val1]
+
+    objcArgumentCls :: Assertion
+    objcArgumentCls =
+        [citem|[somename test];|] @?= [citem|[$recv:(k) $id:("test")];|]
+      where
+        k = [objcmethrecv|somename|]
+
+    objcArgument :: Assertion
+    objcArgument =
+        [citem|[$recv:(k) $kwarg:(p)];|] @?= [citem|[somename doSome:@"string"];|]
+      where
+        k = [objcmethrecv|somename|]
+        p = [objcarg|doSome:@"string"|]
+
+    objcArguments :: Assertion
+    objcArguments =
+        [citem|[$recv:(k) $kwargs:(r)];|]
+        @?= [citem|[somename doSome:@"string" doSomeMore:@"moreStrings"];|]
+      where
+        k = [objcmethrecv|somename|]
+        p = [objcarg|doSome:@"string"|]
+        q = [objcarg|doSomeMore:@"moreStrings"|]
+        r = [p,q]
+
+    objcVarArgument :: Assertion
+    objcVarArgument =
+        [citem|[$recv:(k) $kwarg:(r) $args:(p)];|]
+        @?= [citem|[NSString stringWithFormat:@"A string: %@, a float: %1.2f", @"string", 31415.9265];|]
+      where
+        k = [objcmethrecv|NSString|]
+        r = [objcarg|stringWithFormat:@"A string: %@, a float: %1.2f"|]
+        p = [a, b]
+        a = [cexp|@"string"|]
+        b = [cexp|31415.9265|]
+
+    objcLits :: Assertion
+    objcLits =
+        [cexp|@[$(objcLit "foo"), $(objcLit True), $(objcLit False), $(objcLit 'a'), nil]|]
+        @?= [cexp|@[@"foo", @YES, @NO, @'a', nil]|]

Language/C/Parser/Lexer.x
@@ -123,8 +123,21 @@ c :-
  --
  -- Objective-C
  --
- "$ifdecl:"  / { allowAnti } { lexAnti Tanti_objc_ifdecl }
- "$ifdecls:" / { allowAnti } { lexAnti Tanti_objc_ifdecls }
+ "$ifdecl:"     / { allowAnti } { lexAnti Tanti_objc_ifdecl }
+ "$ifdecls:"    / { allowAnti } { lexAnti Tanti_objc_ifdecls }
+ "$prop:"       / { allowAnti } { lexAnti Tanti_objc_prop }
+ "$props:"      / { allowAnti } { lexAnti Tanti_objc_props }
+ "$propattr:"   / { allowAnti } { lexAnti Tanti_objc_prop_attr }
+ "$propattrs:"  / { allowAnti } { lexAnti Tanti_objc_prop_attrs }
+ "$dictelems:"  / { allowAnti } { lexAnti Tanti_objc_dicts }
+ "$methparam:"  / { allowAnti } { lexAnti Tanti_objc_param }
+ "$methparams:" / { allowAnti } { lexAnti Tanti_objc_params }
+ "$methproto:"  / { allowAnti } { lexAnti Tanti_objc_method_proto }
+ "$methdef:"    / { allowAnti } { lexAnti Tanti_objc_method_def }
+ "$methdefs:"   / { allowAnti } { lexAnti Tanti_objc_method_defs }
+ "$recv:"       / { allowAnti } { lexAnti Tanti_objc_recv }
+ "$kwarg:"      / { allowAnti } { lexAnti Tanti_objc_arg }
+ "$kwargs:"     / { allowAnti } { lexAnti Tanti_objc_args }
 }
 
 <0> {

Language/C/Parser/Parser.y
@@ -232,8 +232,21 @@ import qualified Language.C.Syntax as C
  '__strong'            { L _ T.TObjC__strong }
  '__unsafe_unretained' { L _ T.TObjC__unsafe_unretained }
 
- ANTI_OBJC_IFDECL  { L _ (T.Tanti_objc_ifdecl _) }
- ANTI_OBJC_IFDECLS { L _ (T.Tanti_objc_ifdecls _) }
+ ANTI_OBJC_IFDECL       { L _ (T.Tanti_objc_ifdecl _) }
+ ANTI_OBJC_IFDECLS      { L _ (T.Tanti_objc_ifdecls _) }
+ ANTI_OBJC_PROP         { L _ (T.Tanti_objc_prop _) }
+ ANTI_OBJC_PROPS        { L _ (T.Tanti_objc_props _) }
+ ANTI_OBJC_PROP_ATTR    { L _ (T.Tanti_objc_prop_attr _) }
+ ANTI_OBJC_PROP_ATTRS   { L _ (T.Tanti_objc_prop_attrs _) }
+ ANTI_OBJC_DICTS        { L _ (T.Tanti_objc_dicts _) }
+ ANTI_OBJC_PARAM        { L _ (T.Tanti_objc_param _) }
+ ANTI_OBJC_PARAMS       { L _ (T.Tanti_objc_params _) }
+ ANTI_OBJC_METHOD_PROTO { L _ (T.Tanti_objc_method_proto _) }
+ ANTI_OBJC_METHOD_DEF   { L _ (T.Tanti_objc_method_def _) }
+ ANTI_OBJC_METHOD_DEFS  { L _ (T.Tanti_objc_method_defs _) }
+ ANTI_OBJC_RECV         { L _ (T.Tanti_objc_recv _) }
+ ANTI_OBJC_ARG          { L _ (T.Tanti_objc_arg _) }
+ ANTI_OBJC_ARGS         { L _ (T.Tanti_objc_args _) }
 
  --
  -- CUDA
@@ -299,9 +312,16 @@ import qualified Language.C.Syntax as C
 --
 -- Objective-C
 --
-%name parseObjCProp       objc_property_decl
-%name parseObjCIfaceDecls objc_interface_decl_list
-%name parseObjCImplDecls  objc_implementation_decl_list
+%name parseObjCProp        objc_property_decl
+%name parseObjCIfaceDecls  objc_interface_decl_list
+%name parseObjCImplDecls   objc_implementation_decl_list
+%name parseObjCDictElem    objc_key_value
+%name parseObjCPropAttr    objc_property_attr
+%name parseObjCMethodParam objc_method_param
+%name parseObjCMethodProto objc_method_proto
+%name parseObjCMethodDef   objc_method_definition
+%name parseObjCMethodRecv  objc_receiver
+%name parseObjCKeywordArg  objc_keywordarg
 
 %right NAMED OBJCNAMED
 
@@ -2129,12 +2149,19 @@ block_literal :
  -
  ------------------------------------------------------------------------------}
 
-objc_key_value_rlist :: { RevList (Exp, Exp) }
-objc_key_value_rlist :
+objc_key_value :: { ObjCDictElem }
+objc_key_value :
     assignment_expression ':' assignment_expression
-      { rsingleton ($1, $3) }
-  | objc_key_value_rlist ',' assignment_expression ':' assignment_expression
-      { rcons ($3, $5) $1 }
+      { ObjCDictElem $1 $3 ($1 `srcspan` $3) }
+
+objc_key_value_rlist :: { RevList ObjCDictElem }
+objc_key_value_rlist :
+    objc_key_value
+      { rsingleton $1 }
+  | ANTI_OBJC_DICTS
+      { rsingleton (AntiObjCDictElems (getANTI_OBJC_DICTS $1) (srclocOf $1)) }
+  | objc_key_value_rlist ',' objc_key_value
+      { rcons $3 $1 }
 
 objc_string_literal_rlist :: { RevList Const }
 objc_string_literal_rlist :
@@ -2264,6 +2291,8 @@ objc_receiver :
           Var (Id "super" _) loc -> ObjCRecvSuper loc
           _                      -> ObjCRecvExp $1 (srclocOf $1)
       }
+  | ANTI_OBJC_RECV
+      { AntiObjCRecv (getANTI_OBJC_RECV $1) (srclocOf $1) }
 
 objc_message_args :: { ([ObjCArg], [Exp]) }
 objc_message_args :
@@ -2332,6 +2361,8 @@ objc_keywordarg_rlist :
       { rsingleton $1 }
   | objc_keywordarg_rlist objc_keywordarg
       { $2 `rcons` $1 }
+  | ANTI_OBJC_ARGS
+      { rsingleton (AntiObjCArgs (getANTI_OBJC_ARGS $1) (srclocOf $1)) }
 
 objc_keywordarg :: { ObjCArg }
 objc_keywordarg :
@@ -2339,6 +2370,8 @@ objc_keywordarg :
       { ObjCArg Nothing (Just $2) ($1 `srcspan` $2) }
   | objc_selector ':' assignment_expression
       { ObjCArg (Just $1) (Just $3) ($1 `srcspan` $3) }
+  | ANTI_OBJC_ARG
+      { AntiObjCArg (getANTI_OBJC_ARG $1) (srclocOf $1) }
 
 objc_vararg_rlist :: { RevList Exp }   -- might be empty
 objc_vararg_rlist :
@@ -2346,6 +2379,8 @@ objc_vararg_rlist :
       { rnil }
   | objc_vararg_rlist ',' assignment_expression
       { $3 `rcons` $1 }
+  | ANTI_ARGS
+      { rsingleton (AntiArgs (getANTI_ARGS $1) (srclocOf $1)) }
 
 -- Objective-C extension: at expression
 --
@@ -2597,6 +2632,8 @@ objc_interface_decl_rlist :
       { rcons $2 $1 }
   | objc_interface_decl_rlist ANTI_OBJC_IFDECLS
       { rcons (AntiObjCIfaceDecls (getANTI_OBJC_IFDECLS $2) (srclocOf $2)) $1 }
+  | objc_interface_decl_rlist ANTI_OBJC_PROPS
+      { rcons (AntiObjCProps (getANTI_OBJC_PROPS $2) (srclocOf $2)) $1 }
 
 objc_interface_decl :: { ObjCIfaceDecl }
 objc_interface_decl :
@@ -2617,6 +2654,8 @@ objc_property_decl :
       { ObjCIfaceProp [] $3 ($1 `srcspan` $3) }
   | '@' 'property' '(' objc_property_attr_rlist ')' struct_declaration
       { ObjCIfaceProp (rev $4) $6 ($1 `srcspan` $6) }
+  | ANTI_OBJC_PROP
+      { AntiObjCProp (getANTI_OBJC_PROP $1) (srclocOf $1) }
 
 objc_property_attr_rlist :: { RevList ObjCPropAttr }
 objc_property_attr_rlist :
@@ -2624,6 +2663,8 @@ objc_property_attr_rlist :
       { rsingleton $1 }
   | objc_property_attr_rlist ',' objc_property_attr
       { rcons $3 $1 }
+  | ANTI_OBJC_PROP_ATTRS
+      { rsingleton (AntiObjCAttrs (getANTI_OBJC_PROP_ATTRS $1) (srclocOf $1)) }
 
 objc_property_attr :: { ObjCPropAttr }
 objc_property_attr :
@@ -2654,6 +2695,8 @@ objc_property_attr :
            ; _                        -> expectedObjCPropertyAttr (locOf $1)
            }
       }
+  | ANTI_OBJC_PROP_ATTR
+     { AntiObjCAttr (getANTI_OBJC_PROP_ATTR $1) (srclocOf $1) }
 
 objc_method_requirement :: { ObjCMethodReq }
 objc_method_requirement :
@@ -2674,34 +2717,38 @@ objc_method_proto :
         in
           ObjCMethodProto True res attrs params hasVargs $3 ($1 `srcspan` $3)
       }
+  | ANTI_OBJC_METHOD_PROTO
+      { AntiObjCMethodProto (getANTI_OBJC_METHOD_PROTO $1) (srclocOf $1) }
 
 objc_method_decl :: { (Maybe Type, [Attr], [ObjCParam], Bool) }
 objc_method_decl :
-    attribute_specifiers_opt objc_method_args
+    attribute_specifiers_opt objc_method_params
       { (Nothing, $1, $2, False) }
-  | '(' type_name ')' attribute_specifiers_opt objc_method_args
+  | '(' type_name ')' attribute_specifiers_opt objc_method_params
       { (Just $2, $4, $5, False) }
-  | attribute_specifiers_opt objc_method_args ',' '...'
+  | attribute_specifiers_opt objc_method_params ',' '...'
       { (Nothing, $1, $2, True) }
-  | '(' type_name ')' attribute_specifiers_opt objc_method_args ',' '...'
+  | '(' type_name ')' attribute_specifiers_opt objc_method_params ',' '...'
       { (Just $2, $4, $5, True) }
 
-objc_method_args :: { [ObjCParam] }
-objc_method_args :
+objc_method_params :: { [ObjCParam] }
+objc_method_params :
     objc_selector
       { [ObjCParam (Just $1) Nothing [] Nothing (srclocOf $1)] }
-  | objc_method_arg_rlist
+  | objc_method_param_rlist
       { rev $1 }
 
-objc_method_arg_rlist :: { RevList ObjCParam }
-objc_method_arg_rlist :
-    objc_method_arg
+objc_method_param_rlist :: { RevList ObjCParam }
+objc_method_param_rlist :
+    objc_method_param
       { rsingleton $1 }
-  | objc_method_arg_rlist objc_method_arg
+  | objc_method_param_rlist objc_method_param
       { rcons $2 $1 }
+  | ANTI_OBJC_PARAMS
+      { rsingleton (AntiObjCParams (getANTI_OBJC_PARAMS $1) (srclocOf $1)) }
 
-objc_method_arg :: { ObjCParam }
-objc_method_arg :
+objc_method_param :: { ObjCParam }
+objc_method_param :
     objc_selector ':' '(' type_name ')' attribute_specifiers_opt identifier
       { ObjCParam (Just $1) (Just $4) $6 (Just $7) ($1 `srcspan` $7) }
   |               ':' '(' type_name ')' attribute_specifiers_opt identifier
@@ -2710,6 +2757,8 @@ objc_method_arg :
       { ObjCParam (Just $1) Nothing   $3 (Just $4) ($1 `srcspan` $4) }
   |               ':'                   attribute_specifiers_opt identifier
       { ObjCParam Nothing   Nothing   $2 (Just $3) ($1 `srcspan` $3) }
+  |   ANTI_OBJC_PARAM
+      { AntiObjCParam (getANTI_OBJC_PARAM $1) (srclocOf $1) }
 
 -- Objective-C extension: protocol declaration
 --
@@ -2864,6 +2913,10 @@ objc_method_definition :
         in
           ObjCMethDef $1 stmts ($1 `srcspan` loc)
       }
+  | ANTI_OBJC_METHOD_DEF
+      { AntiObjCMeth (getANTI_OBJC_METHOD_DEF $1) (srclocOf $1) }
+  | ANTI_OBJC_METHOD_DEFS
+      { AntiObjCMeths (getANTI_OBJC_METHOD_DEFS $1) (srclocOf $1) }
 
 -- Objective-C extension: compatibility alias
 --
@@ -2972,8 +3025,21 @@ getANTI_INITS       (L _ (T.Tanti_inits v))       = v
 --
 -- Objective-C
 --
-getANTI_OBJC_IFDECL  (L _ (T.Tanti_objc_ifdecl v))  = v
-getANTI_OBJC_IFDECLS (L _ (T.Tanti_objc_ifdecls v)) = v
+getANTI_OBJC_IFDECL       (L _ (T.Tanti_objc_ifdecl v))       = v
+getANTI_OBJC_IFDECLS      (L _ (T.Tanti_objc_ifdecls v))      = v
+getANTI_OBJC_PROP         (L _ (T.Tanti_objc_prop v))         = v
+getANTI_OBJC_PROPS        (L _ (T.Tanti_objc_props v))        = v
+getANTI_OBJC_PROP_ATTR    (L _ (T.Tanti_objc_prop_attr v))    = v
+getANTI_OBJC_PROP_ATTRS   (L _ (T.Tanti_objc_prop_attrs v))   = v
+getANTI_OBJC_DICTS        (L _ (T.Tanti_objc_dicts v))        = v
+getANTI_OBJC_PARAM        (L _ (T.Tanti_objc_param v))        = v
+getANTI_OBJC_PARAMS       (L _ (T.Tanti_objc_params v))       = v
+getANTI_OBJC_METHOD_PROTO (L _ (T.Tanti_objc_method_proto v)) = v
+getANTI_OBJC_METHOD_DEF   (L _ (T.Tanti_objc_method_def v))   = v
+getANTI_OBJC_METHOD_DEFS  (L _ (T.Tanti_objc_method_defs v))  = v
+getANTI_OBJC_RECV         (L _ (T.Tanti_objc_recv v))         = v
+getANTI_OBJC_ARG          (L _ (T.Tanti_objc_arg v))          = v
+getANTI_OBJC_ARGS         (L _ (T.Tanti_objc_args v))         = v
 
 lexer :: (L T.Token -> P a) -> P a
 lexer cont = do

Language/C/Parser/Tokens.hs
@@ -233,6 +233,19 @@ data Token = Teof
 
            | Tanti_objc_ifdecl String
            | Tanti_objc_ifdecls String
+           | Tanti_objc_prop String
+           | Tanti_objc_props String
+           | Tanti_objc_prop_attr String
+           | Tanti_objc_prop_attrs String
+           | Tanti_objc_dicts String
+           | Tanti_objc_param String
+           | Tanti_objc_params String
+           | Tanti_objc_method_proto String
+           | Tanti_objc_method_def String
+           | Tanti_objc_method_defs String
+           | Tanti_objc_recv String
+           | Tanti_objc_arg String
+           | Tanti_objc_args String
     deriving (Ord, Eq)
 
 instance Pretty Token where
@@ -291,10 +304,26 @@ instance Show Token where
     show (Tanti_init s)                 = "$init:" ++ s
     show (Tanti_inits s)                = "$inits:" ++ s
 
-    show (TObjCnamed s)                 = s
+    --
+    -- Objective C
+    --
+    show (TObjCnamed s)              = s
 
-    show (Tanti_objc_ifdecl s)          = "$ifdecl:" ++ s
-    show (Tanti_objc_ifdecls s)         = "$ifdecls:" ++ s
+    show (Tanti_objc_ifdecl s)       = "$ifdecl:" ++ s
+    show (Tanti_objc_ifdecls s)      = "$ifdecls:" ++ s
+    show (Tanti_objc_prop s)         = "$prop:" ++ s
+    show (Tanti_objc_props s)        = "$props:" ++ s
+    show (Tanti_objc_prop_attr s)    = "$propattr:" ++ s
+    show (Tanti_objc_prop_attrs s)   = "$propattrs:" ++ s
+    show (Tanti_objc_dicts s)        = "$dictelems:" ++ s
+    show (Tanti_objc_param s)        = "$methparam:" ++ s
+    show (Tanti_objc_params s)       = "$methparams:" ++ s
+    show (Tanti_objc_method_proto s) = "$methproto:" ++ s
+    show (Tanti_objc_method_def s)   = "$methdef:" ++ s
+    show (Tanti_objc_method_defs s)  = "$methdefs:" ++ s
+    show (Tanti_objc_recv s)         = "$recv:" ++ s
+    show (Tanti_objc_arg s)          = "$kwarg:" ++ s
+    show (Tanti_objc_args s)         = "$kwargs:" ++ s
 
     show t = fromMaybe (error "language-c-quote: internal error: unknown token")
                        (lookup t tokenStrings)

Language/C/Pretty.hs
@@ -496,6 +496,9 @@ instance Pretty Definition where
         srcloc loc
         <> text "@compatibility_alias" <+> ppr aident <+> ppr cident
 
+    ppr (AntiObjCMeth v _)  = pprAnti "methdef" v
+    ppr (AntiObjCMeths v _) = pprAnti "methdefs" v
+
     pprList ds = stack (map ppr ds) <> line
 
 pprIfaceBody :: [Id] -> [ObjCIvarDecl] -> [ObjCIfaceDecl] -> Doc
@@ -844,6 +847,8 @@ instance Pretty Exp where
         pprMsgArg (ObjCArg Nothing    (Just e) loc) = pprLoc loc $ colon <+> ppr e
         pprMsgArg (ObjCArg _          Nothing  loc)
           = error $ "pretty printing 'ObjCArg': missing expression at " ++ show loc
+        pprMsgArg (AntiObjCArg v _)  = pprAnti "kwarg" v
+        pprMsgArg (AntiObjCArgs v _) = pprAnti "kwargs" v
 
         pprVarArg e = comma <+> ppr e
 
@@ -873,7 +878,7 @@ instance Pretty Exp where
     pprPrec _ (ObjCLitDict as loc) =
         srcloc loc <>
         char '@' <> braces
-          (commasep (map (\(l, r) -> ppr l <+> colon <+> ppr r) as))
+          (commasep (map ppr as))
 
     pprPrec _ (ObjCLitBoxed e loc) =
         srcloc loc <>
@@ -894,6 +899,10 @@ instance Pretty Exp where
     pprPrec _ (AntiArgs v _)  = pprAnti "args"  v
 
     pprPrec _ (AntiExp v _)   = pprAnti "var"  v
+
+instance Pretty ObjCDictElem where
+    pprPrec _ (ObjCDictElem l r _)    = ppr l <+> colon <+> ppr r
+    pprPrec _ (AntiObjCDictElems v _) = pprAnti "dictelems" v
 
 instance Pretty BinOp where
     ppr Add  = text "+"
@@ -992,6 +1001,9 @@ instance Pretty ObjCIfaceDecl where
     ppr (AntiObjCIfaceDecls v _loc) =
         pprAnti "ifdecls" v
 
+    ppr (AntiObjCProp v _)  = pprAnti "prop" v
+    ppr (AntiObjCProps v _) = pprAnti "props" v
+
 instance Pretty ObjCPropAttr where
     ppr (ObjCGetter ident loc)     = pprLoc loc $ text "getter=" <> ppr ident
     ppr (ObjCSetter ident loc)     = pprLoc loc $ text "setter=" <> ppr ident <> colon
@@ -1005,6 +1017,8 @@ instance Pretty ObjCPropAttr where
     ppr (ObjCStrong loc)           = pprLoc loc $ text "strong"
     ppr (ObjCWeak loc)             = pprLoc loc $ text "weak"
     ppr (ObjCUnsafeUnretained loc) = pprLoc loc $ text "unsafe_unretained"
+    ppr (AntiObjCAttr v _)         = pprAnti "propattr" v
+    ppr (AntiObjCAttrs v _)        = pprAnti "propattrs" v
 
 instance Pretty ObjCMethodReq where
     ppr (ObjCRequired _loc) = text "@required"
@@ -1018,6 +1032,8 @@ instance Pretty ObjCParam where
          (Just sid, Nothing) -> ppr sid
          (_       , Just pid)
            -> maybe empty ppr sel <> colon <> maybe empty (parens . ppr) ty <> ppr attrs <> ppr pid
+    ppr (AntiObjCParam p _)  = pprAnti "methparam" p
+    ppr (AntiObjCParams v _) = pprAnti "methparams" v
 
 instance Pretty ObjCMethodProto where
     ppr (ObjCMethodProto isClassMeth resTy attrs1 params vargs attrs2 loc) =
@@ -1030,6 +1046,7 @@ instance Pretty ObjCMethodProto where
         <> spread (map ppr params)
         <> if vargs then text ", ..." else empty
         <> ppr attrs2
+    ppr (AntiObjCMethodProto p _) = pprAnti "methproto" p
 
 instance Pretty ObjCCatch where
     ppr (ObjCCatch Nothing     block loc)  = srcloc loc <> text "@catch (...)" <+> ppr block
@@ -1043,3 +1060,4 @@ instance Pretty ObjCRecv where
     ppr (ObjCRecvExp e loc)               = pprLoc loc $ ppr e
     ppr (ObjCRecvClassName className loc) = pprLoc loc $ ppr className
     ppr (ObjCRecvTypeName typeName loc)   = pprLoc loc $ ppr typeName
+    ppr (AntiObjCRecv v _)                = pprAnti "recv" v

commit 935ef462e9bf6db37a040cc11c2b95bdddf3c60c

Fix antiquotation of ObjC interface declarations.
This fixes #51. The original breakage was caused by a35e6212.

Language/C/Quote/Base.hs
@@ -400,23 +400,20 @@ qqBlockItemListE (C.AntiBlockItems v _ : items) =
 qqBlockItemListE (stm : stms) =
     Just [|$(dataToExpQ qqExp stm) : $(dataToExpQ qqExp stms)|]
 
-qqObjcIfaceDeclE :: [C.ObjCIfaceDecl] -> Maybe (Q Exp)
-qqObjcIfaceDeclE [] = Just [|[]|]
-qqObjcIfaceDeclE (C.AntiObjCIfaceDecls v _ : decls) =
+qqObjcIfaceDeclE :: C.ObjCIfaceDecl -> Maybe (Q Exp)
+qqObjcIfaceDeclE (C.AntiObjCProp p _) = Just $ antiVarE p
+qqObjcIfaceDeclE _                    = Nothing
+
+qqObjcIfaceDeclListE :: [C.ObjCIfaceDecl] -> Maybe (Q Exp)
+qqObjcIfaceDeclListE [] = Just [|[]|]
+qqObjcIfaceDeclListE (C.AntiObjCProps p _ : decls) =
+    Just [|$(antiVarE p) ++ $(dataToExpQ qqExp decls)|]
+qqObjcIfaceDeclListE (C.AntiObjCIfaceDecls v _ : decls) =
     Just [|$(antiVarE v) ++ $(dataToExpQ qqExp decls)|]
-qqObjcIfaceDeclE (C.AntiObjCIfaceDecl v _  : decls) =
+qqObjcIfaceDeclListE (C.AntiObjCIfaceDecl v _  : decls) =
     Just [|$(antiVarE v) : $(dataToExpQ qqExp decls)|]
-qqObjcIfaceDeclE (decl : decls) =
-  Just [|$(dataToExpQ qqExp decl) : $(dataToExpQ qqExp decls)|]
-
-qqObjCPropE :: C.ObjCIfaceDecl -> Maybe (Q Exp)
-qqObjCPropE (C.AntiObjCProp p _) = Just $ antiVarE p
-qqObjCPropE _                    = Nothing
-
-qqObjCPropListE :: [C.ObjCIfaceDecl] -> Maybe (Q Exp)
-qqObjCPropListE [] = Just [|[]|]
-qqObjCPropListE (C.AntiObjCProps p _: props) = Just $ [|$(antiVarE p) ++ $(dataToExpQ qqExp props)|]
-qqObjCPropListE _                            = Nothing
+qqObjcIfaceDeclListE (decl : decls) =
+    Just [|$(dataToExpQ qqExp decl) : $(dataToExpQ qqExp decls)|]
 
 qqObjCPropAttrE :: C.ObjCPropAttr -> Maybe (Q Exp)
 qqObjCPropAttrE (C.AntiObjCAttr pa _) = Just $ antiVarE pa
@@ -494,8 +491,7 @@ qqExp = const Nothing  `extQ` qqStringE
                        `extQ` qqBlockItemE
                        `extQ` qqBlockItemListE
                        `extQ` qqObjcIfaceDeclE
-                       `extQ` qqObjCPropE
-                       `extQ` qqObjCPropListE
+                       `extQ` qqObjcIfaceDeclListE
                        `extQ` qqObjCPropAttrE
                        `extQ` qqObjCPropAttrListE
                        `extQ` qqObjCDictsE

tests/unit/Main.hs
@@ -14,7 +14,7 @@ import qualified Language.C.Syntax as C
 import qualified Language.C.Parser as P
 import MainCPP
 import Numeric (showHex)
-import Objc (objcTests)
+import Objc (objcTests, objcRegressionTests)
 import CUDA (cudaTests)
 import System.Exit (exitFailure, exitSuccess)
 import Text.PrettyPrint.Mainland
@@ -30,6 +30,7 @@ tests = [ constantTests
         , statementCommentTests
         , regressionTests
         , objcTests
+        , objcRegressionTests
         , cudaTests
         ]
 

tests/unit/Objc.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE QuasiQuotes #-}
 
-module Objc (objcTests) where
+module Objc (
+    objcTests,
+    objcRegressionTests
+  ) where
 
 import Test.Framework
 import Test.Framework.Providers.HUnit
@@ -148,3 +151,29 @@ objcTests = testGroup "Objective-C"
     objcLits =
         [cexp|@[$(objcLit "foo"), $(objcLit True), $(objcLit False), $(objcLit 'a'), nil]|]
         @?= [cexp|@[@"foo", @YES, @NO, @'a', nil]|]
+
+objcRegressionTests :: Test
+objcRegressionTests = testGroup "Objective-C Regressions"
+    [ testCase "Issue #51" issue51 ]
+  where
+    issue51 :: Assertion
+    issue51 = [cunit|
+          @interface $id:prefixedClassName : NSObject
+          $ifdecls:ifaceDecls
+          @end
+        |]
+
+        @?= [cunit|
+          @interface dummyClass : NSObject
+          - (void) foo:(int)str fo:(int)str1;
+          + (int) test1:(int)str2;
+          @end
+        |]
+      where
+        prefixedClassName :: String
+        prefixedClassName = "dummyClass"
+
+        ifaceDecls = [objcifdecls|
+            - (void) foo:(int)str fo:(int)str1;
+            + (int) test1:(int)str2;
+            |]