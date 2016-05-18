mainland/language-c-quote

commit d44b984bfe0f0281e58621efc57d799c6250dee0

Fix pretty-print of dangling else statements.
We previously didn't always pretty-print dangling else's in nested if
statements such that they associated with the proper if. Now we do.

To ensure that prop_ParsePrintUnitId holds, we now also normalize parsed
statement blocks so that if they contain a single statement, they parse
as a single statement rather than a statement block consisting of a
single statement. This required some other fixes in the pretty printer.

In practice, we never hit this problem in parsed code, but we could
potentially encounter it in generated code.

Language/C/Parser/Parser.y
@@ -490,10 +490,7 @@ primary_expression :
   | '(' expression_nlt error
       {% unclosed ($1 <--> $2) "(" }
   | '(' compound_statement ')'
-      { let Block items _ = $2
-        in
-          StmExpr items ($1 `srcspan` $3)
-      }
+      { StmExpr (mkBlockItems $2) ($1 `srcspan` $3) }
   | ANTI_ESC
       { AntiEscExp (getANTI_ESC $1) (srclocOf $1) }
   | ANTI_EXP
@@ -758,10 +755,7 @@ primary_expression_nlt :
   | '(' expression_nlt error
       {% unclosed ($1 <--> $2) "(" }
   | '(' compound_statement ')'
-      { let Block items _ = $2
-        in
-          StmExpr items ($1 `srcspan` $3)
-      }
+      { StmExpr (mkBlockItems $2) ($1 `srcspan` $3) }
   | ANTI_ESC
       { AntiEscExp (getANTI_ESC $1) (srclocOf $1) }
   | ANTI_EXP
@@ -2074,9 +2068,9 @@ labeled_statement :
 compound_statement :: { Stm }
 compound_statement:
     '{' begin_scope end_scope '}'
-      { Block [] ($1 `srcspan` $4) }
+      { mkBlock [] ($1 `srcspan` $4) }
   | '{' begin_scope block_item_list end_scope '}'
-      { Block $3 ($1 `srcspan` $5) }
+      { mkBlock $3 ($1 `srcspan` $5) }
   | '{' begin_scope error
       {% unclosed (locOf $3) "{" }
 
@@ -2220,7 +2214,7 @@ function_definition :
     declaration_specifiers declarator compound_statement
       {% do{ let (dspec, declRoot)   =  $1
            ; let (ident, declToDecl) =  $2
-           ; let Block blockItems _  =  $3
+           ; let blockItems          =  mkBlockItems $3
            ; let decl                =  declToDecl declRoot
            ; case decl of
                { Proto protoDecl args _ -> return $
@@ -2240,7 +2234,7 @@ function_definition :
       {% do{ let (dspec, declRoot)   =  $1
            ; let (ident, declToDecl) =  $2
            ; let argDecls            =  $3
-           ; let Block blockItems _  =  $4
+           ; let blockItems          =  mkBlockItems $4
            ; let decl                =  declToDecl declRoot
            ; case decl of
                { OldProto protoDecl args _ -> return $
@@ -2448,23 +2442,22 @@ block_literal :: { Exp }
 block_literal :
     '^'                                              attribute_specifiers_opt compound_statement
       {% do { assertBlocksEnabled ($1 <--> $3) "To use blocks, enable the blocks language extension"
-            ; let Block items _ = $3
+            ; let items = mkBlockItems $3
             ; return $ BlockLit (BlockVoid (srclocOf $1)) $2 items ($1 `srcspan` $3)
             }
       }
   | '^' '(' parameter_list ')'                       attribute_specifiers_opt compound_statement
       {% do { assertBlocksEnabled ($1 <--> $6) "To use blocks, enable blocks language extension"
-            ; let Block items _ = $6
+            ; let items = mkBlockItems $6
             ; return $ BlockLit (BlockParam $3 ($2 `srcspan` $4)) $5 items ($1 `srcspan` $6)
             }
       }
   | '^' specifier_qualifier_list abstract_declarator attribute_specifiers_opt compound_statement
       {% do { assertBlocksEnabled ($1 <--> $5) "To use blocks, enable blocks language extension"
-            ; let { decl          = $3 (declRoot $2)
-                  ; Block items _ = $5
-                  }
-            ; dspec <- mkDeclSpec $2
-            ; let typeLoc = dspec `srcspan` decl
+            ; let decl    =  $3 (declRoot $2)
+            ; let items   =  mkBlockItems $5
+            ; dspec       <- mkDeclSpec $2
+            ; let typeLoc =  dspec `srcspan` decl
             ; return $ BlockLit (BlockType (Type dspec decl typeLoc) typeLoc) $4 items ($1 `srcspan` $5)
             }
       }
@@ -2527,15 +2520,15 @@ objc_selector_rlist :
 objc_at_statement :: { Stm }
 objc_at_statement :
     '@' 'try' compound_statement objc_catch_statement_rlist '@' 'finally' compound_statement
-      { let { Block tryItems _     = $3
-            ; Block finallyItems _ = $7
+      { let { tryItems     = mkBlockItems $3
+            ; finallyItems = mkBlockItems $7
             }
         in
          ObjCTry tryItems (rev $4) (Just finallyItems) ($1 `srcspan` $7)
       }
   | '@' 'try' compound_statement objc_catch_statement_rlist
-      {% do { let { Block tryItems _ = $3
-                  ; catchStmts       = rev $4
+      {% do { let { tryItems   = mkBlockItems $3
+                  ; catchStmts = rev $4
                   }
             ; when (null catchStmts) $
                 throw $ ParserException ($1 <--> $3) $
@@ -2556,12 +2549,12 @@ objc_at_statement :
   | '@' 'throw' error
       {% expected ["';'", "expression"] Nothing }
   | '@' 'synchronized' '(' expression ')' compound_statement
-      { let Block items _ = $6
+      { let items = mkBlockItems $6
         in
          ObjCSynchronized $4 items ($1 `srcspan` $6)
       }
   | '@' 'autoreleasepool' compound_statement
-      { let Block items _ = $3
+      { let items = mkBlockItems $3
         in
          ObjCAutoreleasepool items ($1 `srcspan` $3)
       }
@@ -2571,11 +2564,11 @@ objc_catch_statement_rlist :
     {- empty -}
       { rnil }
   | objc_catch_statement_rlist '@' 'catch' '(' parameter_declaration ')' compound_statement
-      { let Block items _ = $7
+      { let items = mkBlockItems $7
         in
         rcons (ObjCCatch (Just $5) items ($2 `srcspan` $7)) $1 }
   | objc_catch_statement_rlist '@' 'catch' '(' '...' ')' compound_statement
-      { let Block items _ = $7
+      { let items = mkBlockItems $7
         in
         rcons (ObjCCatch Nothing items ($2 `srcspan` $7)) $1 }
 
@@ -3224,14 +3217,14 @@ property_dynamic :
 objc_method_definition :: { Definition }
 objc_method_definition :
     objc_method_proto semi compound_statement
-      { let Block stmts loc = $3
+      { let stmts = mkBlockItems $3
         in
-          ObjCMethDef $1 stmts ($1 `srcspan` loc)
+          ObjCMethDef $1 stmts ($1 `srcspan` $3)
       }
-  | objc_method_proto     compound_statement
-      { let Block stmts loc = $2
+  | objc_method_proto      compound_statement
+      { let stmts = mkBlockItems $2
         in
-          ObjCMethDef $1 stmts ($1 `srcspan` loc)
+          ObjCMethDef $1 stmts ($1 `srcspan` $2)
       }
   | ANTI_OBJC_METHOD_DEF
       { AntiObjCMeth (getANTI_OBJC_METHOD_DEF $1) (srclocOf $1) }
@@ -3259,7 +3252,7 @@ objc_compatibility_alias :
 cuda_lambda_expression :: { Exp }
 cuda_lambda_expression : cuda_lambda_introducer cuda_lambda_declarator compound_statement
     {% do { assertCudaEnabled ($1 <--> $3) "To use lambda-expressions, enable support for CUDA"
-          ; let Block items _ = $3
+          ; let items = mkBlockItems $3
           ; return $ Lambda $1 $2 items ($1 `srcspan` $3)
           }
     }
@@ -4000,6 +3993,14 @@ assertCudaEnabled loc errMsg = do
  cuda_enabled <- useCUDAExts
  unless cuda_enabled $
   throw $ ParserException loc $ text errMsg
+
+mkBlock :: [BlockItem] -> SrcLoc -> Stm
+mkBlock [BlockStm stm] _    = stm
+mkBlock items          sloc = Block items sloc
+
+mkBlockItems :: Stm -> [BlockItem]
+mkBlockItems (Block items _) = items
+mkBlockItems stm             = [BlockStm stm]
 
 mkCommentStm :: L T.Token -> Stm -> Stm
 mkCommentStm tok stm = Comment (getCOMMENT tok) stm (srclocOf tok)

Language/C/Pretty.hs
@@ -555,36 +555,40 @@ instance Pretty Stm where
     ppr (If test then' maybe_else sloc) =
         srcloc sloc <>
         text "if" <+> parens (ppr test) <>
-        pprStm then' (fmap pprElse maybe_else)
+        pprThen then' (fmap pprElse maybe_else)
       where
-        pprElse :: Stm -> Doc
-        pprElse else' = text "else" <> pprStm else' Nothing
+        pprThen :: Stm -> Maybe Doc -> Doc
+        pprThen stm@(Block {}) rest        = space <> ppr stm <+> maybe empty id rest
+        pprThen stm@(If {})    rest        = space <> ppr [BlockStm stm] <+> maybe empty id rest
+        pprThen stm            Nothing     = nest 4 (line <> ppr stm)
+        pprThen stm            (Just rest) = nest 4 (line <> ppr stm) </> rest
 
-        pprStm :: Stm -> Maybe Doc -> Doc
-        pprStm stm@(Block {}) rest        = space <> ppr stm <+> maybe empty id rest
-        pprStm stm@(If {})    rest        = space <> ppr stm <+> maybe empty id rest
-        pprStm stm            Nothing     = nest 4 (line <> ppr stm)
-        pprStm stm            (Just rest) = nest 4 (line <> ppr stm) </> rest
+        pprElse :: Stm -> Doc
+        pprElse stm =
+            text "else" <> go stm
+          where
+            go :: Stm -> Doc
+            go (Block {}) = space <> ppr stm
+            go (If {})    = space <> ppr stm
+            go _stm       = nest 4 (line <> ppr stm)
 
     ppr (Switch e stm sloc) =
         srcloc sloc <>
-        text "switch" <+> parens (ppr e) <+/> ppr stm
+        text "switch" <+> parens (ppr e) <> pprBlock stm
 
     ppr (While e stm sloc) =
         srcloc sloc <>
-        text "while" <+> parens (ppr e) <+/> ppr stm
+        text "while" <+> parens (ppr e) <> pprBlock stm
 
     ppr (DoWhile stm e sloc) =
         srcloc sloc <>
-        text "do" <+/> ppr stm <+/> text "while" <> parens (ppr e) <> semi
+        text "do" <> pprBlock stm <+/> text "while" <> parens (ppr e) <> semi
 
     ppr (For ini test post stm sloc) =
         srcloc sloc <>
         text "for" <+>
         (parens . semisep) [either ppr ppr ini, ppr test, ppr post] <>
-        case stm of
-          Block {} -> space <> ppr stm
-          _ -> nest 4 $ line <> ppr stm
+        pprBlock stm
 
     ppr (Goto ident sloc) =
         srcloc sloc <>
@@ -681,6 +685,11 @@ instance Pretty Stm where
         srcloc sloc
         <>  text "@autoreleasepool"
         </> ppr block
+
+pprBlock :: Stm -> Doc
+pprBlock stm@(Block {}) = space <> ppr stm
+pprBlock stm@(If {})    = space <> ppr [BlockStm stm]
+pprBlock stm            = nest 4 $ line <> ppr stm
 
 instance Pretty BlockItem where
     ppr (BlockDecl decl) = ppr decl <> semi

commit d954d0606b0bc7162d023eb75a0d1666d5e84a25

Fix issue #68.
This bug was introduced in d44b984b.

Language/C/Parser/Parser.y
@@ -3995,8 +3995,9 @@ assertCudaEnabled loc errMsg = do
   throw $ ParserException loc $ text errMsg
 
 mkBlock :: [BlockItem] -> SrcLoc -> Stm
-mkBlock [BlockStm stm] _    = stm
-mkBlock items          sloc = Block items sloc
+mkBlock items@[BlockStm AntiStms{}] sloc = Block items sloc
+mkBlock [BlockStm stm]              _    = stm
+mkBlock items                       sloc = Block items sloc
 
 mkBlockItems :: Stm -> [BlockItem]
 mkBlockItems (Block items _) = items

tests/unit/Main.hs
@@ -7,6 +7,7 @@ import Test.Framework.Providers.HUnit
 import Test.HUnit (Assertion, (@?=))
 
 import qualified Data.ByteString.Char8 as B
+import Data.Char (isSpace)
 import Data.Loc (SrcLoc, noLoc, startPos)
 import Control.Exception (SomeException)
 import Language.C.Quote.C
@@ -430,7 +431,8 @@ statementCommentTests = testGroup "Statement comments"
 
 regressionTests :: Test
 regressionTests = testGroup "Regressions"
-    [ issue64
+    [ issue68
+    , issue64
     , testCase "pragmas" test_pragmas
     , issue48
     , testCase "Issue #44" issue44
@@ -457,6 +459,15 @@ regressionTests = testGroup "Regressions"
                       ]
                       noLoc
             ]
+
+    issue68 :: Test
+    issue68 = testCase "Issue #68"$
+        simpleRender (ppr [cstm|if (!initialized) { $stms:init_stms }|])
+        @?=
+        "if (!initialized) { return; }"
+      where
+        init_stms :: [C.Stm]
+        init_stms = [[cstm|return;|]]
 
     issue64 :: Test
     issue64 = testGroup "Issue #64"
@@ -541,3 +552,12 @@ regressionTests = testGroup "Regressions"
                          C.Type (C.DeclSpec [] [] (C.Tlong_double_Imaginary noLoc) noLoc)
                                 (C.DeclRoot noLoc)
                                 noLoc
+
+-- | Render a document as a single line.
+simpleRender :: Doc -> String
+simpleRender doc =
+    map space2space (displayS (renderCompact doc) "")
+  where
+    space2space :: Char -> Char
+    space2space c | isSpace c = ' '
+                  | otherwise = c