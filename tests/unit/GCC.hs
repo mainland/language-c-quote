710f5dc508d57a65018c488cd2f011854a4fc5f6
Mark Barbone <mark.l.barbone@gmail.com>
2020-12-06 16:57:52 -0800

    Add tests for case ranges

--- tests/unit/GCC.hs
+++ tests/unit/GCC.hs
@@ -6,13 +6,14 @@ module GCC (
 
 import Test.Framework
 import Test.Framework.Providers.HUnit
-import Test.HUnit (Assertion, (@?=))
+import Test.HUnit (Assertion, assert, (@?=))
 
 import qualified Data.ByteString.Char8 as B
 import Data.Char (isSpace)
 import Data.Loc (SrcLoc, noLoc, startPos)
 import Control.Exception (SomeException)
 import Language.C.Quote.GCC
+import Language.C.Smart ()
 import qualified Language.C.Syntax as C
 import qualified Language.C.Parser as P
 import Text.PrettyPrint.Mainland
@@ -23,6 +24,8 @@ gccTests = testGroup "GCC attribute quotations"
     [ testCase "attr antiquote" test_attr
     , testCase "attrs antiquote" test_attrs
     , testCase "attrs antiquote pretty" test_attr_p
+    , testCase "case ranges quote" test_case_ranges
+    , testCase "case ranges pretty" test_case_ranges_p
     ]
   where
     test_attr :: Assertion
@@ -47,3 +50,12 @@ gccTests = testGroup "GCC attribute quotations"
     test_attr_p :: Assertion
     test_attr_p =
       pretty 80 (ppr [cattr|section(".sram2")|]) @?= "section(\".sram2\")"
+
+    test_case_ranges :: Assertion
+    test_case_ranges = assert $ case [cstm| case 10 ... 20: ; |] of
+      C.CaseRange 10 20 (C.Exp Nothing _) _ -> True
+      _ -> False
+
+    test_case_ranges_p :: Assertion
+    test_case_ranges_p =
+      pretty 80 (ppr [cstm| case 10 ... 20: ; |]) @?= "\ncase 10 ... 20:\n;"
