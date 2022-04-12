836b552771f672f4c8498aaccb5d236d5e490ed3
pema99 <pemamalling@gmail.com>
2022-04-12 02:09:19 +0200

    More tests

--- tests/unit/ISPC.hs
+++ tests/unit/ISPC.hs
@@ -18,19 +18,25 @@ import Text.PrettyPrint.Mainland
 import Text.PrettyPrint.Mainland.Class
 
 ispcTests :: Test
-ispcTests = testGroup "ISPC land"
+ispcTests = testGroup "ISPC"
     [ testCase "ispc unmasked" test_unmasked_qualifier
-    ,testCase "ispc unmasked wvars" test_unmasked_qualifier_wvars
-    ,testCase "ispc foreach" test_foreach
-    ,testCase "ispc multidimensional foreach" test_foreach_multiple
-    ,testCase "ispc foreach_active" test_foreach_active
-    ,testCase "ispc foreach_tiled" test_foreach_tiled
-    ,testCase "ispc multidimensional foreach_tiled" test_foreach_tiled_multiple
-    ,testCase "ispc uniform" test_uniform
-    ,testCase "ispc varying" test_varying
-    ,testCase "ispc export" test_export
-    ,testCase "ispc extern" test_extern
-    ,testCase "ispc foreach_unique" test_foreach_unique
+    , testCase "ispc unmasked wvars" test_unmasked_qualifier_wvars
+    , testCase "ispc foreach" test_foreach
+    , testCase "ispc multidimensional foreach" test_foreach_multiple
+    , testCase "ispc foreach_active" test_foreach_active
+    , testCase "ispc foreach_tiled" test_foreach_tiled
+    , testCase "ispc multidimensional foreach_tiled" test_foreach_tiled_multiple
+    , testCase "ispc uniform" test_uniform
+    , testCase "ispc varying" test_varying
+    , testCase "ispc export" test_export
+    , testCase "ispc extern" test_extern
+    , testCase "ispc foreach_unique" test_foreach_unique
+    , testCase "ispc pointer qualifiers" test_pointer_quals
+    , testCase "ispc coherent if" test_cif
+    , testCase "ispc coherent while" test_cwhile
+    , testCase "ispc coherent for" test_cfor
+    , testCase "ispc coherent do" test_cdo
+    , testCase "ispc foreach iters" test_foreachiters
     ]
 
 test_foreach :: Assertion
@@ -105,3 +111,40 @@ test_extern = pretty 80 (ppr decl) @?= expected
   where
     decl = [cedecl|extern "C" void func(int a, float b);|]
     expected = "extern \"C\" void func(int a, float b);"
+
+test_pointer_quals :: Assertion
+test_pointer_quals = pretty 80 (ppr decl) @?= expected
+  where
+    decl = [cedecl|uniform int * varying a;|]
+    expected = "uniform int *varying a;"
+
+test_cif :: Assertion
+test_cif = pretty 80 (ppr decl) @?= expected
+  where
+    decl = [cstm|cif (cond) { int a = 10; }|]
+    expected = "cif (cond) {\n    int a = 10;\n}"
+
+test_cwhile :: Assertion
+test_cwhile = pretty 80 (ppr decl) @?= expected
+  where
+    decl = [cstm|cwhile (cond) { int a = 10; }|]
+    expected = "cwhile (cond) {\n    int a = 10;\n}"
+
+test_cfor :: Assertion
+test_cfor = pretty 80 (ppr decl) @?= expected
+  where
+    decl = [cstm|cfor (uniform int i = 0; i < 5; i++) { int a = 10; }|]
+    expected = "cfor (uniform int i = 0; i < 5; i++) {\n    int a = 10;\n}"
+
+test_cdo :: Assertion
+test_cdo = pretty 80 (ppr decl) @?= expected
+  where
+    decl = [cstm|cdo { int a = 10; } while (cond);|]
+    expected = "cdo {\n    int a = 10;\n} while(cond);"
+
+test_foreachiters :: Assertion
+test_foreachiters = pretty 80 (ppr decl) @?= expected
+  where
+    iters = [ispcforeachiters|i = 5 ... n, j = 8 ... m|]
+    decl = [cstm|foreach ($foreachiters:iters, k = 0 ... l) { int a = 10; }|]
+    expected = "foreach (i = 5 ... n, j = 8 ... m, k = 0 ... l) {\n    int a = 10;\n}"
